94f4f846f443a1365757553c0eac6f7aab864de3
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-06 16:13:51 +0330

    Brew formula update for fing version v0.2.3

--- Formula/fing.rb
+++ Formula/fing.rb
@@ -5,20 +5,20 @@
 class Fing < Formula
   desc ""
   homepage "https://fing.ir"
-  version "0.2.2"
+  version "0.2.3"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/fingcloud/cli/releases/download/v0.2.2/fing-darwin-amd64.tar.gz"
-      sha256 "44bffb5a26e836db26f7ba7ea6052cc5e1723815d147928219606ce06b21413a"
+      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-darwin-amd64.tar.gz"
+      sha256 "4122d18fe9cf3056d5083368db0605858f9d20d326461def23b2591d6b038192"
 
       def install
         bin.install "fing"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/fingcloud/cli/releases/download/v0.2.2/fing-darwin-arm64.tar.gz"
-      sha256 "d24990ff677e3f0bcd3ee5a504525875c01684c33a08a514a1bfa4cb71533395"
+      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-darwin-arm64.tar.gz"
+      sha256 "6a44a6a71f249a34ff23b67fd1b17dec5040c37be081f542d9bce2b210e4d4ff"
 
       def install
         bin.install "fing"
@@ -28,16 +28,16 @@ class Fing < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/fingcloud/cli/releases/download/v0.2.2/fing-linux-arm64.tar.gz"
-      sha256 "e2de2e210c67b34cccb66e589eab15f0deb16d22e610abdd9242b7274cd7e093"
+      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-linux-arm64.tar.gz"
+      sha256 "237a0601e498388d3d0fd31eb28cdb3d0591273d861c905a26151e1df8f47855"
 
       def install
         bin.install "fing"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/fingcloud/cli/releases/download/v0.2.2/fing-linux-amd64.tar.gz"
-      sha256 "7058d4d45fba0a60785ed089cfc1a90e5af463c11746dad03b3c3a3966e6185b"
+      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-linux-amd64.tar.gz"
+      sha256 "a6347ab2b71e43f27e58ae33e990c6ef47a0da7f1aa0eae7286385611ace10cd"
 
       def install
         bin.install "fing"
