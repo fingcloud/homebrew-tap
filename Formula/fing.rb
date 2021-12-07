e635a7167960d73c9527f2f6daa70e4f4b92e2c9
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-07 10:01:37 +0330

    Brew formula update for fing version v0.2.3

--- Formula/fing.rb
+++ Formula/fing.rb
@@ -8,17 +8,17 @@ class Fing < Formula
   version "0.2.3"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-darwin-amd64.tar.gz"
-      sha256 "6f33865a6b30c410124ecd787230dbc93e136243329987df2b88904373e7d1c9"
+    if Hardware::CPU.arm?
+      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-darwin-arm64.tar.gz"
+      sha256 "26ef02e9c1a40c0acecb6d6274c8c83448c541ff5541b8748f57c319573a7957"
 
       def install
         bin.install "fing"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-darwin-arm64.tar.gz"
-      sha256 "52716fda8178e420fd876f26f4f91e8f0999cd17697f1f9444ed1a4e506115d7"
+    if Hardware::CPU.intel?
+      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-darwin-amd64.tar.gz"
+      sha256 "33f522b93c9b103f867c888d35d82e818442d9e3e25299d4672c0e8f7739a44c"
 
       def install
         bin.install "fing"
@@ -27,17 +27,17 @@ class Fing < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-linux-amd64.tar.gz"
-      sha256 "f2b924ed48759afa2b16f6b4632052da43041bbd81e9e20addbb20ea473720f7"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-linux-arm64.tar.gz"
+      sha256 "75ac99df118c1954f4236a8faf507f60e9e08769961a3342873fca43b5524c4d"
 
       def install
         bin.install "fing"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-linux-arm64.tar.gz"
-      sha256 "fb8c006ac8abefb33d869704e12fbf2ba21b801f0b84df1559bed44763f0a2e7"
+    if Hardware::CPU.intel?
+      url "https://github.com/fingcloud/cli/releases/download/v0.2.3/fing-linux-amd64.tar.gz"
+      sha256 "451a223c77e2d8ae0905a13660f9c1c5bee61301a6cc0bddc338514cc8a42ab7"
 
       def install
         bin.install "fing"
